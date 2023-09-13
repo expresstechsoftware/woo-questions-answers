22ed09018a6b7fb5adf34c273dffb1e5d41ebb84
Younes DRO <dro66_8@hotmail.fr>
2023-09-13 17:36:29 +0100

    Update WooCommerce 8.1

--- ets_question_answer.php
+++ ets_question_answer.php
@@ -7,26 +7,28 @@
  * Author: ExpressTech Software Solutions Pvt. Ltd.
  * Author URI: https://www.expresstechsoftwares.com
  * Requires at least: 5.6
- * WC tested up to: 8.0.2
+ * WC tested up to: 8.1
  * Requires PHP: 7.0
  * Text Domain: ets_q_n_a
  * Domain Path: /languages
- */   
- 
-if ( ! defined( 'ABSPATH' ) ) exit; 
+ */
 
-define('ETS_WOO_QA_PATH', plugin_dir_url( __FILE__ ));
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+define( 'ETS_WOO_QA_PATH', plugin_dir_url( __FILE__ ) );
 
 class ETS_WOO_PRODUCT_QUESTION_ANSWER {
-	public function __construct() { 
-		add_action( 'init', array($this, 'qa_load_local'));	
-		require ( 'includes/ets_admin_qa_function.php' );
-		require( 'includes/ets_user_qa_function.php' );	
-	}  
+	public function __construct() {
+		add_action( 'init', array( $this, 'qa_load_local' ) );
+		require 'includes/ets_admin_qa_function.php';
+		require 'includes/ets_user_qa_function.php';
+	}
 
 	public function qa_load_local() {
-		$localeDir = dirname(plugin_dir_path( __FILE__ ) ) . '/question-answer-plugin-by-ets/languages/ets_q_n_a-'.get_locale() .'.mo';
-        $res = load_textdomain( 'ets_q_n_a', $localeDir );   
-    }
-} 
+		$localeDir = dirname( plugin_dir_path( __FILE__ ) ) . '/question-answer-plugin-by-ets/languages/ets_q_n_a-' . get_locale() . '.mo';
+		$res       = load_textdomain( 'ets_q_n_a', $localeDir );
+	}
+}
 $etsWooProductQuestionAnswer = new ETS_WOO_PRODUCT_QUESTION_ANSWER();
